8088977eef1917698c171a3f2ac6d4285755e150
skryuchkov <skryuchkov@gemsdev.com>
2025-02-03 14:20:36 +0600

    not working

--- DotNet/GrpcClient/Program.cs
+++ DotNet/GrpcClient/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Grpc.Net.Client;
+using GrpcServer;
 
-Console.WriteLine("Hello, World!");
+namespace GrpcClient;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+        var client = new Greeter.GreeterClient(channel);
+
+        var reply = await client.SayHelloAsync(new HelloRequest() { Name = "World!" });
+        
+        Console.WriteLine(reply.Message);
+        Console.ReadKey();
+    }
+}
